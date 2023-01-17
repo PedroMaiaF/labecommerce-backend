e85f28322cc4905229d7ea908bd6438db827adc4
PedroMaiaF <pedromaiaf21@gmail.com>
2023-01-17 16:40:04 -0400

    Exercícios da tarde feitos na branch aprofundamento-sql

--- labecommerce.db/labecommerce.sql
+++ labecommerce.db/labecommerce.sql
@@ -1,5 +1,6 @@
--- Active: 1673873950449@@127.0.0.1@3306
+-- Active: 1673975619818@@127.0.0.1@3306
 
+-- SQL dia 1 - tarefas da tarde
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
@@ -11,6 +12,7 @@ PRAGMA table_info ('users');
 INSERT INTO users (id, email, password)
 VALUES ('Pedro Maia', 'pedro@email.com', 'estudante2'),
 ('Carlos Henrique', 'henrique@email.com', 'estudante1'),
+('João Paulo', 'joaopaulo@email.com', 'estudante5'),
 ('Carlos Bergson', 'bergson@email', 'estudante3');
 
 SELECT * FROM users;
@@ -32,3 +34,54 @@ VALUES ('J01', 'League of Legends', 00, 'games'),
 ('C03', 'XBox Series X', 3700, 'video games');
 
 SELECT * FROM products;
+
+-- SQL dia 2 - tarefas da tarde
+SELECT * FROM products
+WHERE name LIKE '%PS5';
+
+INSERT INTO users (id, email, password)
+VALUES ('Laís Rodrigues', 'laisrodrigues@email.com', 'estudante4');
+SELECT * FROM users;
+
+INSERT INTO products(id, name, price, category)
+VALUES ('J03', 'Mario Kart', 250,'games'),
+('J04', 'Mario Party Ultimate', 299.00,'games');
+
+SELECT * FROM products;
+
+SELECT * FROM products
+WHERE id = 'J04';
+
+SELECT (id) as idUsuario FROM users
+WHERE id ='João Paulo';
+
+DELETE FROM products
+WHERE id = 'J04';
+
+DELETE FROM users
+WHERE id = 'João Paulo';
+
+UPDATE users
+SET id = 'Cristiano Ronaldo'
+WHERE id = "João Paulo";
+
+UPDATE products
+SET name = 'Cristiano Ronaldo'
+WHERE id = "João Paulo";
+
+SELECT * FROM users
+ORDER BY email ASC;
+
+SELECT * FROM products
+ORDER BY price ASC
+LIMIT 2 OFFSET 0;
+
+SELECT * FROM products
+WHERE price >= 10 AND price <= 4500
+ORDER BY price ASC;
+
+
+-- Dar baixa na tabela
+DROP TABLE users;
+
+DROP TABLE products;
